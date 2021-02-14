15590df413d2b0d3fb43ae3b6c20657bf6df794e
indra-pryg <79032318+indra-pryg@users.noreply.github.com>
2021-02-14 11:53:11 +0700

    Monitoring Denyut Jantung dan Suhu

---
+++ Monitoring Denyut Jantung dan Suhu/UI/UI Pasien/Unit2.pas
@@ -0,0 +1,48 @@
+unit Unit2;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ComCtrls, Vcl.ExtCtrls,
+  Vcl.Imaging.pngimage;
+
+type
+  TForm2 = class(TForm)
+    GroupBox1: TGroupBox;
+    GroupBox2: TGroupBox;
+    PageControl1: TPageControl;
+    TabSheet1: TTabSheet;
+    PageControl2: TPageControl;
+    TabSheet2: TTabSheet;
+    PageControl3: TPageControl;
+    TabSheet3: TTabSheet;
+    PageControl4: TPageControl;
+    TabSheet4: TTabSheet;
+    PageControl5: TPageControl;
+    TabSheet5: TTabSheet;
+    PageControl6: TPageControl;
+    TabSheet6: TTabSheet;
+    Image1: TImage;
+    Image2: TImage;
+    Image3: TImage;
+    Image4: TImage;
+    Image5: TImage;
+    Image6: TImage;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+end.
